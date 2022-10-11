a01f08a7e4fe081ef773c184800bfd6dd6f241f9
mainhu111 <115562791+mainhu111@users.noreply.github.com>
2022-10-11 19:30:42 +0530

    code changed

--- Exam Distribution.py
+++ Exam Distribution.py
@@ -6,11 +6,14 @@ def fact(n):
 
 def wrong(n):
     s=1
-    for i in range(1,n+1):
+    i=1
+    while i<n+1:
         s+=((-1)**i)/fact(i)
+        i=i+1
     return fact(n)*s
 
-n= int(input())
+n= int(input("Enter the Value of N: "))
 t=fact(n)
 m=int(wrong(n))
 print(t-m)
+print("Program Ended:")
