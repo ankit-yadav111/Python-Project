4b774070e4b880ada0f5e0bedede24867702f4f9
mainhu111 <115562791+mainhu111@users.noreply.github.com>
2022-10-11 19:26:38 +0530

    Added extra details and changed the code

--- Pallindrone count.py
+++ Pallindrone count.py
@@ -4,10 +4,10 @@ def palindrome(st):
         return 1
     return 0
 
-n=int(input())
-st=input()
-m=int(input())
-m_list = input().split()
+n=int(input("Enter the value of N: "))
+st=input("Enter the String: ")
+m=int(input("Enter the value of M: "))
+m_list = input("Enter the value of List Element: ").split()
 count=0
 for i in m_list:
     i=int(i)
@@ -15,4 +15,4 @@ for i in m_list:
         if palindrome(st[j:i+j:1]):
             count+=1
 print(count)
-        
+print("Program Ended:")
