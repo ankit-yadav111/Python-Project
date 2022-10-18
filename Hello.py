13f807e9d8836a8c7702d107c212016fe8f6f4fe
Shreshth0109 <97122887+Shreshth0109@users.noreply.github.com>
2022-10-18 21:11:10 +0530

    Improved the code

--- Hello.py
+++ Hello.py
@@ -1,8 +1,8 @@
 import sys
-print("Hello!, How are you...")
-print(f"File name is {sys.argv[0]}")
-print(f"Total no. of arguments = {len(sys.argv)-1}")
-print("The value of Arguments are: ")
+print("HELLO!, HOW ARE YOU...")
+print(f"FILE NAME IS {sys.argv[0]}")
+print(f"TOTAL NUMBER OF ARGUMENTS = {len(sys.argv)-1}")
+print("The VALUE OF ARGUMENTS: ")
 for i in sys.argv:
    print(i)
 print("Program Ended")
